3db832a4dccdec9e27065679849687bdae38bde5
tusharshaha <tusharshaha128@gmail.com>
2023-05-26 22:02:42 +0600

    created top section for message

--- components/home/MessageSection.tsx
+++ components/home/MessageSection.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const MessageSection: React.FC= () => {
+const MessageSection: React.FC = () => {
   return (
     <div className='grow'>
-      
+      {/* top bar  */}
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-2'>
+          <span>icon</span>
+          <div>
+            <h4>Mijanur Rahaman</h4>
+            <span className='font-normal text-slate-500'>Active Now</span>
+          </div>
+        </div>
+        <div className='flex gap-2 items-center'>
+          <span>call</span>
+          <span>video</span>
+        </div>
+      </div>
     </div>
   );
 };
