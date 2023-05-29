e743c48478197c0c0badbc0b35efda826d9b9f4c
tusharshaha <tusharshaha128@gmail.com>
2023-05-29 11:31:09 +0600

    updated user list

--- components/home/UserListSection.tsx
+++ components/home/UserListSection.tsx
@@ -1,36 +1,39 @@
+import Image from 'next/image';
 import React from 'react';
 
 const UserListSection: React.FC = () => {
   const userList = [
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
-    {icon: "icon", name: "Tushar Kumar Shaha", message: "new message"},
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
+    { name: "Tushar Kumar Shaha", message: "new message" },
   ]
   return (
-    <div className='w-25 max-h-screen overflow-y-auto scroll-smooth scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-blue-300 p-4'>
+    <div className='w-25 max-h-screen overflow-y-auto scroll-smooth scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-blue-300 py-4 px-2'>
       <div className='flex flex-col gap-4'>
         {
-          userList.map((ele, i)=> <div key={i} className='flex items-center gap-2'>
-            <span>{ele.icon}</span>
+          userList.map((ele, i) => <div key={i} className='flex items-center gap-2 rounded p-2 hover:bg-zinc-700 cursor-pointer'>
+            <div className='w-[50px] h-[50px] rounded-full overflow-hidden'>
+              <Image src='/avatar.jpeg' height={70} width={70} alt="avatar" />
+            </div>
             <div>
               <h5>{ele.name}</h5>
               <span className='font-normal text-slate-300'>{ele.message}</span>
