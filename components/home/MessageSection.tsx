de338c55f639eec5c0d8d9acf686ee06fdc781ea
tusharshaha <tusharshaha128@gmail.com>
2023-05-29 11:15:47 +0600

    added avatar on messege profile

--- components/home/MessageSection.tsx
+++ components/home/MessageSection.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { ImAttachment } from "react-icons/im";
 import { BsEmojiSmile } from "react-icons/bs";
-import { IoSend } from "react-icons/io5";
+import { IoSend, IoCall, IoVideocam } from "react-icons/io5";
+import Image from 'next/image';
 
 const MessageSection: React.FC = () => {
   return (
     <div className='grow max-h-screen flex flex-col justify-between'>
       {/* top bar  */}
-      <div className='flex items-center justify-between border-b border-blue-400 sticky top-0'>
-        <div className='flex items-center gap-2 px-4 py-2'>
-          <span>icon</span>
+      <div className='flex items-center px-4 py-2 justify-between border-b border-slate-500 sticky top-0'>
+        <div className='flex items-center gap-2'>
+          <div className='w-[50px] h-[50px] rounded-full overflow-hidden'>
+            <Image src='/avatar.jpeg' height={70} width={70} alt="avatar" />
+          </div>
           <div>
             <h4>Mijanur Rahaman</h4>
             <span className='font-normal text-slate-300'>Active Now</span>
           </div>
         </div>
-        <div className='flex gap-2 items-center'>
-          <span>call</span>
-          <span>video</span>
+        <div className='flex gap-6 items-center text-2xl text-blue-400'>
+          <button><IoCall /></button>
+          <button><IoVideocam /></button>
         </div>
       </div>
       {/* bottom bar  */}
