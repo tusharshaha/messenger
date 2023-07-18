60111981b441356b53d6fcc9a68d0649c5a8b225
tusharshaha <tusharshaha128@gmail.com>
2023-07-18 22:17:23 +0600

    created axios service

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { WebsocketProvider, socket } from '@/context/websocket.context'
 import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider, } from '@tanstack/react-query';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return <WebsocketProvider value={socket}>
-    <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   </WebsocketProvider>
 
 }
